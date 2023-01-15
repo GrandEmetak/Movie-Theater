5eadb66356e347c603ffe1b8f3bc16dedfc1e9bf
GrandEmetak <JekHamond@tutanota.com>
2023-01-15 13:47:31 +0500

    fix: updated info in to comments

--- src/main/java/movie/theater/servlet/HallServlet.java
+++ src/main/java/movie/theater/servlet/HallServlet.java
@@ -1,33 +1,23 @@
-package ru.job4j.cinema.servlet;
+package movie.theater.servlet;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import ru.job4j.cinema.model.Place;
-import ru.job4j.cinema.store.PsqlStore;
+import movie.theater.model.Place;
+import movie.theater.store.PsqlStore;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Сервлет получает данные о текущес состоянии мест кинотеатра (заняты/свободны)
+ * Сервлет получает данные о текущем состоянии мест кинотеатра (заняты/свободны)
  * places busy or free
- * в случае необходимости передачи через json массива использовать
- *         int time = 0;
- *         for (int i = 0; i < 3; i++) {
- *             for (int j = 0; j < 3; j++) {
- *                 arrPlace[i][j] = placeList.get(time++);
- *             }
- *         }
- * String json = GSON.toJson(arrPlace);
  */
 /*@WebServlet(urlPatterns = "/hall.do")*/
 public class HallServlet extends HttpServlet {
